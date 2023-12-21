d42a4b4887f6a05a1bd7d97b1691e2dc7b30b970
Milad Joodi <winbeta.com@gmail.com>
2023-12-22 02:18:05 +0330

    Update Nav.jsx

--- 02.Dashboard-AP/src/Components/Nav/Nav.jsx
+++ 02.Dashboard-AP/src/Components/Nav/Nav.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './Nav.css'
 import { AiFillCaretLeft } from "react-icons/ai";
+import { HiOutlineUsers, HiListBullet } from "react-icons/hi2";
+import { TbLogin } from "react-icons/tb";
+import { FiUserPlus } from "react-icons/fi";
+import { BsPatchCheck } from "react-icons/bs";
 
 
 function Nav() {
@@ -11,6 +15,7 @@ function Nav() {
                 <img src="./images/joodi.jpg" className='w-14 rounded-full' />
             </div>
             <nav className='pt-6'>
+                {/* sidebar items start */}
                 <div>
                     <a href="#" className='w-full font-thin text-blue-500 flex items-center p-4 my-2 transition-colors duration-200  bg-gradient-to-l from-white to-blue-100 border-l-4 border-blue-500'>
                         <span className='text-left'>
@@ -18,14 +23,39 @@ function Nav() {
                         </span>
                         <span className='mx-4 font-normal text-lg'>داشبورد</span>
                     </a>
-                    <a href="#" className='w-full font-thin text-blue-500 flex items-center p-4 my-2 transition-colors duration-200  bg-gradient-to-l from-white to-blue-100 border-l-4 border-blue-500'>
-                        <span className='text-left'>
-                            <AiFillCaretLeft />
+                    <a href="#" className='w-full font-thin text-gray-500 flex items-center p-4 my-2 transition-colors duration-200 justify-start hover:text-blue-500'>
+                        <span >
+                            <HiOutlineUsers size={17} />
                         </span>
-                        <span className='mx-4 font-normal text-lg'>داشبورد</span>
+                        <span className='mx-4 font-normal text-lg'>کاربران</span>
+                    </a>
+                    <a href="#" className='w-full font-thin text-gray-500 flex items-center p-4 my-2 transition-colors duration-200 justify-start hover:text-blue-500'>
+                        <span >
+                            <HiListBullet size={20} />
+                        </span>
+                        <span className='mx-4 font-normal text-lg'>فرم ها</span>
+                    </a>
+                    <a href="#" className='w-full font-thin text-gray-500 flex items-center p-4 my-2 transition-colors duration-200 justify-start hover:text-blue-500'>
+                        <span >
+                            <TbLogin size={20} />
+                        </span>
+                        <span className='mx-4 font-normal text-lg'>لاگین</span>
+                    </a>
+                    <a href="#" className='w-full font-thin text-gray-500 flex items-center p-4 my-2 transition-colors duration-200 justify-start hover:text-blue-500'>
+                        <span >
+                            <FiUserPlus size={19} />
+                        </span>
+                        <span className='mx-4 font-normal text-lg'>لاگین</span>
                     </a>
                 </div>
+                {/* /sidebar items end */}
             </nav>
+            <div className='absolute bottom-0 my-10'>
+                <a href="#" className='text-gray-500 hover:text-gray-800 transition-colors duration-200 flex items-center py-2 px-8'>
+                    <BsPatchCheck className='text-blue-600' />
+                <span className='mx-4'>نسخه 2</span>
+                </a>
+            </div>
         </div>
     </div>
   )
