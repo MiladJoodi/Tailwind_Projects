7386caab3a2f6a83a5b12643b6f5b8366510b1e5
Milad Joodi <winbeta.com@gmail.com>
2023-12-23 21:08:31 +0330

    ♥

--- 02.Dashboard-AP/src/Pages/Forms/Forms.jsx
+++ 02.Dashboard-AP/src/Pages/Forms/Forms.jsx
@@ -32,8 +32,32 @@ function Forms() {
                             ایمیل
                             <span className="text-red-500 required-dot">*</span>
                           </label>
+                          <input type="email" 
+                          className="ring-red-500 ring-2 rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-300 text-base focus:ring-2 focus:ring-purple-600 focus:border-transparent focus:outline-none" 
+                          placeholder="ایمیل شما" />
+                          <p className="absolute text-sm text-red-500 -bottom-6">
+                            ایمیل شما نامعتبر است
+                          </p>
                         </div>
                       </div>
+                      <div className="w-full">
+                          <div className="relative">
+                              <label className="text-gray-700">
+                                  نام
+                              </label>
+                              <input type="text" id="name"
+                              className="rounded-lg border-transparent flex-1 appearance-none border border-gray-400 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-300 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                              />
+                          </div>
+                        </div>
+            {/* buttons */}
+                        <div className="">
+                            <span className="block w-full rounded-md shadow-sm">
+                              <button type="submit" className="py-2 px-4 bg-indigo-400 focus:ring-indigo-500 focus:ring-offset-indigo-500 text-white w-full transition ease-out duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg">ثبت</button>
+                            </span>
+                        </div>
+            {/* buttons */}
+
                     </div>
                   </div>
                 </div>
