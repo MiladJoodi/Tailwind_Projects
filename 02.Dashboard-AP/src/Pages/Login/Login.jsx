1018c4dae7be739e4178403549b12ff797362224
Milad Joodi <winbeta.com@gmail.com>
2023-12-23 22:01:27 +0330

    ♥

--- 02.Dashboard-AP/src/Pages/Login/Login.jsx
+++ 02.Dashboard-AP/src/Pages/Login/Login.jsx
@@ -7,20 +7,25 @@ function Login() {
   return (
     <div className='content w-full'>
     <Header />
-    <div className='w-full h-screen flex justify-center items-center'>
-    <div className='flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10'>
+
+    <div className="container mx-auto p-4 sm:px-8 max-w-8xl">
+          <div className="py-8">
+            <div className="flex flex-col mb-1 sm:mb:0 justify-between w-full">
+              <h2 className="text-2xl leading-tight">ورود و اعتبار سنجی</h2>
+              <div className='flex flex-col w-full mx-auto  mt-8 max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10'>
       <div className='self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl dark:text-white'>
         ورود به پنل کاربری
       </div>
 
       <div className='mt-8'>
         <form className='' autoComplete='off'>
-            <div className='flex flex-col mb-2'>
+            <div className='flex flex-col هفثئ mb-2'>
               <div className='flex relative'>
-                <span className='rounded-r-md inline-flex items-center px-3 border-t border-r border-b border-gray-300 text-gray-300 shadow-sm text-sm'>
+                <span className='rounded-r-md inline-flex items-center px-3 border-t border-r border-b border-gray-400 text-gray-400 shadow-sm text-sm'>
                   <MdEmail />
                 </span>
-                <input type="email" className='rounded-l-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent' />
+                <input type="email" placeholder='ایمیل شما'
+                className='rounded-l-lg flex-1 appearance-none border border-gray-400 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent' />
               </div>
             </div>
         </form>
@@ -28,8 +33,14 @@ function Login() {
 
     </div>
     </div>
+
+              </div>
+              </div>
+              </div>
+
+
+    
    
-  </div>
   )
 }
 
